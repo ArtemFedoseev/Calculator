a5fa0db124cb3a26070a0b6b9b7a1643d3a540d0
ArtemFedoseev <58297238+ArtemFedoseev@users.noreply.github.com>
2020-01-08 18:05:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { evaluate } from 'mathjs';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const buttons = ['<-', '(',')','^','7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+']
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      expression: '',
+      result: ''
+    }
+  }
+
+  handleInput = (event) => {
+    let expression = event.target.value.replace(/=|;|:|@|[^=+-^*./)(]/,'');
+    this.evalExpression(expression);
+  }
+
+  handleKeyPress = (key) => {
+    let expression = this.state.expression;
+    let newExpression = '';
+    if (key != '<-') {
+      newExpression = expression+key;
+    } else if (key != '=') {
+      newExpression = expression.slice(0,expression.length-1);
+    } else {
+      return;
+    }
+    this.evalExpression(newExpression);
+  }
+
+  evalExpression = (expression) => {
+    try {
+      let result = evaluate(expression);
+      this.setState({expression, result})
+    } catch (err) {
+        this.setState({expression, result: "INVALID EXPRESSION"})
+      }
+  }
+
+  render(){
+    const {expression, result} = this.state;
+    return(<div className="container">
+        <div className="wrapper">
+          <input type="text" value={expression} pattern={"[0-9]*"} onChange={this.handleInput}/>
+          <p>Result: {result}</p>
+          {buttons.map(el => <div className="button" key={el}>
+                                <button onClick={() => this.handleKeyPress(el)}>
+                                  {el}
+                                </button>
+                                {(buttons.indexOf(el)+1)%4 === 0 ? <br/> : ''}
+                              </div>
+                              )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
